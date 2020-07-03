d4b1de11e24e6c616b4bc4938766ef8dfbbd4cca
Admin <xuanloc4858@gmail.com>
2020-07-03 14:50:24 +0700

    layoutLoc

---
+++ app/src/main/java/com/example/englishtester/fragment_question.java
@@ -0,0 +1,14 @@
+package com.example.englishtester;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fragment_question extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_question);
+    }
+}

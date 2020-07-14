1d4f3a239f36e9294486a80fb2683d13fdb7e10c
Vuon Minh Nguyen <54706711+minhvuon@users.noreply.github.com>
2020-07-14 09:21:06 +0700

    Update LoginActivity.java

--- app/src/main/java/com/example/englishtester/LoginActivity.java
+++ app/src/main/java/com/example/englishtester/LoginActivity.java
@@ -6,6 +6,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+
+import android.renderscript.ScriptGroup;
+import android.text.InputType;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
